136110efd4bd5ca04db90dfd9bf0a13af2f315c5
Josh Harvey <josh@lamassu.is>
2014-03-24 13:57:46 -0400

    added queryString option

--- test/jsonquest-test.js
+++ test/jsonquest-test.js
@@ -2,6 +2,7 @@ var http = require('http'),
     assert = require('assert'),
     cb = require('assert-called'),
     jsonquest = require('../');
+var querystring = require('querystring');
 
 var server = http.createServer(cb(function (req, res) {
   var data = '';
@@ -112,3 +113,41 @@ var server3 = http.createServer(cb(function (req, res) {
     server3.close();
   }));
 }));
+
+var server4 = http.createServer(cb(function (req, res) {
+  var data = '';
+
+  assert.equal(req.method, 'PUT');
+  assert.equal(req.url, '/hello');
+  assert.equal(req.headers['content-type'], 'application/x-www-form-urlencoded');
+  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
+
+  req.on('data', function (chunk) {
+    data += chunk.toString('utf8');
+  });
+
+  req.on('end', function () {
+    assert.deepEqual(querystring.parse(data), { hello: 'world', witaj: 'świecie' });
+
+    res.writeHead(201, { 'content-type': 'application/json' });
+    res.write(JSON.stringify({ world: 'hello', świecie: 'witaj' }));
+    res.end();
+  });
+})).listen(9996, cb(function () {
+  jsonquest({
+    host: 'localhost',
+    port: 9996,
+    path: '/hello',
+    body: { hello: 'world', witaj: 'świecie' },
+    method: 'PUT',
+    protocol: 'http',
+    auth: 'user:pass',
+    requestEncoding: 'queryString'
+  }, cb(function (err, res, body) {
+    assert.equal(res.statusCode, 201);
+    assert.deepEqual(body, { world: 'hello', świecie: 'witaj' });
+    assert.equal(res.headers['content-type'], 'application/json');
+    server4.close();
+  }));
+}));
+
