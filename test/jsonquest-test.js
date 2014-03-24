1a4205646a39ba15e3f2aa3ed07190735109ee82
Josh Harvey <josh@lamassu.is>
2014-03-24 12:36:10 -0400

    strong TLS defaults, tweaked error handling

--- test/jsonquest-test.js
+++ test/jsonquest-test.js
@@ -27,7 +27,7 @@ var server = http.createServer(cb(function (req, res) {
     host: 'localhost',
     port: 9999,
     path: '/hello',
-    body: { hello: 'world', witaj: 'świecie' },
+    body: { hello: 'world', witaj: 'świecie', },
     method: 'PUT',
     protocol: 'http',
     auth: 'user:pass'
@@ -37,5 +37,78 @@ var server = http.createServer(cb(function (req, res) {
     assert.equal(res.headers['content-type'], 'application/json');
 
     server.close();
+  }));
+}));
+
+var http = require('http'),
+    assert = require('assert'),
+    cb = require('assert-called'),
+    jsonquest = require('../');
+
+var server2 = http.createServer(cb(function (req, res) {
+  var data = '';
+
+  assert.equal(req.method, 'PUT');
+  assert.equal(req.url, '/hello');
+  assert.equal(req.headers['content-type'], 'application/json');
+  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
+
+  req.on('data', function (chunk) {
+    data += chunk.toString('utf8');
+  });
+
+  req.on('end', function () {
+    assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
+
+    res.writeHead(201, { 'content-type': 'application/json' });
+    res.write(JSON.stringify({err: 'This is an error' }));
+    res.end();
+  });
+})).listen(9998, cb(function () {
+  jsonquest({
+    host: 'localhost',
+    port: 9998,
+    path: '/hello',
+    body: { hello: 'world', witaj: 'świecie', },
+    method: 'PUT',
+    protocol: 'http',
+    auth: 'user:pass'
+  }, cb(function (err, res, body) {
+    assert(err);
+    server2.close();
+  }));
+}));
+
+var server3 = http.createServer(cb(function (req, res) {
+  var data = '';
+
+  assert.equal(req.method, 'PUT');
+  assert.equal(req.url, '/hello');
+  assert.equal(req.headers['content-type'], 'application/json');
+  assert.equal(req.headers.authorization, 'Basic dXNlcjpwYXNz');
+
+  req.on('data', function (chunk) {
+    data += chunk.toString('utf8');
+  });
+
+  req.on('end', function () {
+    assert.deepEqual(JSON.parse(data), { hello: 'world', witaj: 'świecie' });
+
+    res.writeHead(201, { 'content-type': 'application/json' });
+    res.write('This is a string');
+    res.end();
+  });
+})).listen(9997, cb(function () {
+  jsonquest({
+    host: 'localhost',
+    port: 9997,
+    path: '/hello',
+    body: { hello: 'world', witaj: 'świecie', },
+    method: 'PUT',
+    protocol: 'http',
+    auth: 'user:pass'
+  }, cb(function (err, res, body) {
+    assert(err);
+    server3.close();
   }));
 }));
